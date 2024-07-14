b938d4ba51f77255646f26320d6d82354c23a7b9
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-14 20:10:50 +0200

    Add percent blue bar

--- day02/script.js
+++ day02/script.js
@@ -30,4 +30,9 @@ function update() {
       circle.classList.remove("active");
     }
   });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
 }
