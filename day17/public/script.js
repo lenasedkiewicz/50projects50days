7a5d54ac2fc9ffeb86ef4dfed8db55e6d4d258da
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-24 19:25:42 +0200

    Approach to fix connection

--- day17/public/script.js
+++ day17/public/script.js
@@ -1,5 +1,5 @@
 fetch("/config")
-  // .then((response) => response.json())
+  .then((response) => response.json())
   .then((config) => {
     /* Set variables */
     const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${config.apiKey}&page=1`;
@@ -7,61 +7,61 @@ fetch("/config")
     const SEARCH_API = `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=${config.apiKey}&query="`;
 
     getMovies(API_URL);
+
+    async function getMovies(url) {
+      const res = await fetch(url);
+      const data = await res.json();
+      showMovies(data.results);
+    }
+
+    const main = document.getElementById("main");
+
+    function showMovies(movies) {
+      main.innetHTML = "";
+      movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+      });
+      const movieEl = document.createElement("div");
+      movieEl.classList.add("movie");
+
+      movieEl.innetHTML = `
+      <img src="${IMAGE_PATH + poster_path}" alt="${title}"
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview"
+        <h3>Overview</h3>
+        ${overview}
+      </div>`;
+
+      main.appendChild(movieEl);
+    }
+
+    function getClassByRate(vote) {
+      if (vote >= 8) {
+        return "green";
+      } else if (vote >= 5) {
+        return "orange";
+      } else {
+        return "red";
+      }
+    }
+
+    const form = document.getElementById("form");
+    const search = document.getElementById("search");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const searchTerm = search.value;
+
+      if (searchTerm && searchTerm !== "") {
+        getMovies(SEARCH_API + searchTerm);
+
+        search.value = "";
+      } else {
+        window.location.reload();
+      }
+    });
   })
   .catch((error) => console.error("Error fetching API key:", error));
-
-async function getMovies(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  showMovies(data.results);
-}
-
-const main = document.getElementById("main");
-
-function showMovies(movies) {
-  main.innetHTML = "";
-  movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
-  });
-  const movieEl = document.createElement("div");
-  movieEl.classList.add("movie");
-
-  movieEl.innetHTML = `
-  <img src="${IMAGE_PATH + poster_path}" alt="${title}"
-  <div class="movie-info">
-    <h3>${title}</h3>
-    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-  </div>
-  <div class="overview"
-    <h3>Overview</h3>
-    ${overview}
-  </div>`;
-
-  main.appendChild(movieEl);
-}
-
-function getClassByRate(vote) {
-  if (vote >= 8) {
-    return "green";
-  } else if (vote >= 5) {
-    return "orange";
-  } else {
-    return "red";
-  }
-}
-
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const searchTerm = search.value;
-
-  if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
-
-    search.value = "";
-  } else {
-    window.location.reload();
-  }
-});
