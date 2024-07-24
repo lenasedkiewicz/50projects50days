7b67f09142ffa9f945fec530f4b7efccafa85145
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-24 15:28:39 +0200

    Add Js for big cup filling

--- day16/script.js
+++ day16/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
+updateBigCup();
+
 smallCups.forEach((cup, index) => {
   cup.addEventListener("click", () => highlightCups(index));
 });
@@ -26,4 +28,29 @@ function highlightCups(index) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+    remained.style.visibility = "visible";
+    remained.style.height = "10px";
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+    remained.style.visibility = "visible";
+    remained.style.height = "10px";
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  }
 }
