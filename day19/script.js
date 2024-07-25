5d1fc67c5f9477eff218fd15cc2b4ca035031df5
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-25 23:22:34 +0200

    Set clock time

--- day19/script.js
+++ day19/script.js
@@ -19,6 +19,44 @@ const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 
+function setCurrentTime() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setCurrentTime();
+
+setInterval(setCurrentTime, 1000);
+
 /* Set mode */
 
 const toggle = document.querySelector(".toggle");
