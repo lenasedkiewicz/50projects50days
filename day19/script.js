81d782b3e393a416ed7150c0bc28411f03500404
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-25 23:38:57 +0200

    Add date display; update for locale

--- day19/script.js
+++ day19/script.js
@@ -18,35 +18,53 @@ addCurrentDate();
 const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
 
 function setCurrentTime() {
   const currentTime = new Date();
-  const hours = currentTime.getHours();
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "Europe/London", // Specify the desired time zone
+  };
+
+  // Get the time in the specified time zone
+  const formattedTime = currentTime.toLocaleString("en-GB", options);
+
+  // Extract the parts you need
+  const [time, ampm] = formattedTime.split(" ");
+
+  // Split the time into hours, minutes, and seconds
+  const [hours, minutes, seconds] = time.split(":");
   const hoursForClock = hours % 12;
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
     0,
-    11,
+    12,
     0,
     360
   )}deg)`;
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     minutes,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     seconds,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
+
+  timeEl.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm.toUpperCase()}`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
