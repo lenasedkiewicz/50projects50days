e522c245803b978e4f439308672cf53e77348b5d
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-30 19:07:58 +0200

    Add basic JS structure for drag n drop

---
+++ day21/script.js
@@ -0,0 +1,29 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+function dragStart() {
+  console.log("startr");
+}
+
+function dragEnd() {
+  console.log("startr");
+}
+
+function dragOver() {
+  console.log("startr");
+}
+
+function dragEnter() {
+  console.log("startr");
+}
+
+function dragLeave() {
+  console.log("startr");
+}
+
+function dragDrop() {
+  console.log("startr");
+}
