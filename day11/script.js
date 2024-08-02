394b231b3346894fc14e3a2546b88a10d81d8075
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-02 21:59:01 +0200

    Code refactoring - keycodes

--- day11/script.js
+++ day11/script.js
@@ -1,4 +1,4 @@
-function showKeyCodes() {
+function showKeyCodes(e) {
   const insert = document.getElementById("insert");
 
   const keyCodes = {
@@ -6,6 +6,20 @@ function showKeyCodes() {
     "e.keyCode": e.keyCode,
     "e.code": e.code,
   };
+
+  for (let key in keyCodes) {
+    const div = document.createElement("div");
+    div.className = "key";
+    const small = document.createElement("small");
+
+    const keyText = document.createTextNode(key);
+    const valueText = document.createTextNode(keyCodes[key]);
+
+    small.appendChild(keyText);
+    div.appendChild(valueText);
+    div.appendChild(small);
+    insert.appendChild(div);
+  }
 }
 
-window.addEventListener("keydown", (e) => showKeyCodes);
+window.addEventListener("keydown", (e) => showKeyCodes(e));
