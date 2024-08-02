8497e08f033e3f9e14be1d37bf12f7d74fd99847
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-02 21:47:24 +0200

    Code refactoring - keykodes

--- day11/script.js
+++ day11/script.js
@@ -1,17 +1,11 @@
-const insert = document.getElementById("insert");
+function showKeyCodes() {
+  const insert = document.getElementById("insert");
 
-window.addEventListener("keydown", (e) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${e.key === " " ? "Space" : e.key}
-    <small>event.key</small>
-  </div>
-  <div class="key">
-  ${e.keyCode}
-    <small>event.keyCode</small>
-  </div>
-  <div class="key">
-  ${e.code}
-    <small>event.code</small>
-  </div>`;
-});
+  const keyCodes = {
+    "e.key": e.key === " " ? "Space" : e.key,
+    "e.keyCode": e.keyCode,
+    "e.code": e.code,
+  };
+}
+
+window.addEventListener("keydown", (e) => showKeyCodes);
