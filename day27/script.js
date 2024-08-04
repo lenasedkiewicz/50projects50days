47b482a824d7892cc9f03919295e29eeabbb8bd5
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-04 22:01:05 +0200

    Add custom logic for message types draw

--- day27/script.js
+++ day27/script.js
@@ -1,15 +1,31 @@
 const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
 
-const messages = [];
+const messages = [
+  { message: "This is an informational message.", class: "info" },
+  { message: "Task completed successfully!", class: "success" },
+  { message: "An error occurred.", class: "error" },
+  { message: "Neutral message without a class." },
+  { message: "Another informational message.", class: "info" },
+  { message: "Operation successful.", class: "success" },
+  { message: "Something went wrong.", class: "error" },
+  { message: "Regular message." },
+  { message: "Informative update.", class: "info" },
+  { message: "Process finished without errors.", class: "success" },
+];
+
+let messageNumber;
 
 button.addEventListener("click", () => createNotification());
 
-function createNotification() {
+function createNotification(message = null) {
   const notification = document.createElement("div");
   notification.classList.add("toast");
 
-  notification.innerText = getRandomMessage();
+  notification.innerText = message ? message : getRandomMessage();
+
+  if (getRandomMessageType() !== "")
+    notification.classList.add(getRandomMessageType());
 
   toasts.appendChild(notification);
 
@@ -19,5 +35,10 @@ function createNotification() {
 }
 
 function getRandomMessage() {
-  return messages[Math.floor(Math.random() * messages.length)];
+  messageNumber = Math.floor(Math.random() * messages.length);
+  return messages[messageNumber].message;
+}
+
+function getRandomMessageType() {
+  return messages[messageNumber].class ? messages[messageNumber].class : "";
 }
