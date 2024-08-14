ddc9d2676dcc28a13ead9420b4c95fa2f204fee4
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-14 13:31:54 +0200

    Add animation JS code

---
+++ day34/script.js
@@ -0,0 +1,24 @@
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
+
+runAnimation();
+
+function runAnimation() {
+  nums.forEach((num, index) => {
+    const nexToLast = nums.length - 1;
+
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && index !== nexToLast) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      } else {
+        counter.classList.add("hide");
+        finalMessage.classList.add("show");
+      }
+    });
+  });
+}
