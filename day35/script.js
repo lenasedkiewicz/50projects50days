22ed5f664b521af434fa6c57358eec748833b986
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-14 15:56:35 +0200

    Add basic animation for slide change

---
+++ day35/script.js
@@ -0,0 +1,23 @@
+const imgs = document.getElementById("imgs");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+const img = document.querySelectorAll("#imgs img");
+
+let index = 0;
+let interval = setInterval(run, 4000);
+
+function run() {
+  index++;
+
+  changeImage();
+}
+
+function changeImage() {
+  if (index > img.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-index * 500}px)`;
+}
